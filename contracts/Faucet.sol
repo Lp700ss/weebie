c106a73b02a56711a3232d8fbeaa35262b54e4d6
Lp700s <siddhant@studio137.co.za>
2022-05-23 14:24:12 +0530

    truffle dev

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-
 contract Faucet {
-    // storage variables
-    uint public funds = 1000;  //positive values only
-    int public counter = -10;
-    uint32 public test = 4294967295;
+    // this is a special function
+    // it's called when you make a tx that doesn't specify
+    // function name to call
+
+    // External function are part of the contract interface
+    // which means they can be called via contracts and other txs
+
+    receive() external payable {}
 }
